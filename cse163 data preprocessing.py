32ec06d512020fda5f6a8573ba0668194795013a
孙小乔 <sunxiaoqiao@sunxiaoqiaodeMacBook-Air.local>
2022-03-06 23:34:23 -0800

    First question added

--- cse163 data preprocessing.py
+++ cse163 data preprocessing.py
@@ -7,8 +7,12 @@ Created on Sat Mar  5 17:15:08 2022
 """
 import os
 import pandas as pd
+import plotly.express as px
+from plotly.offline import plot
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
-directory_csv = '/Users/hsy/Desktop/CSE163/163finalproj/csv_files'
+directory_csv = 'csv_files'
 filenames_csv = os.listdir(directory_csv)
 filenames_csv.sort()
 df_list = []
@@ -25,7 +29,6 @@ for filename in filenames_csv:
 
 meta_data = df_list[-1]
 
-
 # get the names and index of companies that are in the IT industry
 IT_data = meta_data[meta_data['Industry'] == 'IT']
 IT_series = IT_data['Symbol']
@@ -38,6 +41,42 @@ TCS = df_list[IT_series[IT_series == 'TCS'].index[0]]
 TECHM = df_list[IT_series[IT_series == 'TECHM'].index[0]]
 WIPRO = df_list[IT_series[IT_series == 'WIPRO'].index[0]]
 
+# filter data for pre-Covid 19 time
+def before_covid(df):
+    before_covid = df['Date'] <= '2019-12-31'
+    df = df.loc[before_covid]
+    return df
+
+HCLTECH = before_covid(HCLTECH)
+
 # Dropping unuseful columns and rows
 HCLTECH = HCLTECH[['Date', 'Open', 'High', 'Low', 'Last', \
                    'Close', 'VWAP', 'Volume', 'Turnover', 'Trades']]
+
+# First research question: trend of stock HCLTECH's close price and volume over
+# the years
+fig = make_subplots(specs=[[{"secondary_y": True}]])
+fig.add_trace(go.Scatter(x=HCLTECH['Date'],
+                         y=HCLTECH['Close'],
+                         name='Close Price',
+                         mode="lines"),
+              secondary_y=True)
+
+fig.add_trace(go.Bar(x=HCLTECH['Date'],
+                     y=HCLTECH['Volume'],
+                     name='Volume'),
+              secondary_y=False)
+
+fig.update_xaxes(title_text='Year')
+
+# Set y-axes titles and plot title
+fig.update_yaxes(title_text='Close Price', secondary_y=False)
+fig.update_yaxes(title_text='Volume', secondary_y=True)
+fig.update_layout(title={'text': "HCLTECH's Close Price & Volume Over Time",
+                         'y':0.9,
+                         'x':0.5,
+                         'xanchor': 'center',
+                         'yanchor': 'top'})
+plot(fig)
+
+# Find the largest single-day increase and single-day decrease in close price
