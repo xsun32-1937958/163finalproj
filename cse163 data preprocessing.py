54387ec77feb749bee25aaa06d895273581ebe47
孙小乔 <sunxiaoqiao@sunxiaoqiaodeMacBook-Air.local>
2022-03-07 01:51:00 -0800

    question 2 added

--- cse163 data preprocessing.py
+++ cse163 data preprocessing.py
@@ -40,14 +40,24 @@ INFY = df_list[IT_series[IT_series == 'INFY'].index[0]]
 TCS = df_list[IT_series[IT_series == 'TCS'].index[0]]
 TECHM = df_list[IT_series[IT_series == 'TECHM'].index[0]]
 WIPRO = df_list[IT_series[IT_series == 'WIPRO'].index[0]]
+# get a combined IT industry dataframe
+IT_df_temp = [HCLTECH, INFY, TCS, TECHM, WIPRO]
+IT_df = pd.concat(IT_df_temp)
 
-# filter data for pre-Covid 19 time
-def before_covid(df):
-    before_covid = df['Date'] <= '2019-12-31'
-    df = df.loc[before_covid]
+# filter data for year 2014-2019 (covid-19 imapct eliminated)
+def set_time(df):
+    time_mask = (df['Date'] <= '2019-12-31') & (df['Date'] >= '2014-1-1')
+    df = df[time_mask]
     return df
 
-HCLTECH = before_covid(HCLTECH)
+HCLTECH = set_time(HCLTECH)
+INFY = set_time(INFY)
+TCS = set_time(TCS)
+TECHM = set_time(TECHM)
+WIPRO = set_time(WIPRO)
+
+IT_df = set_time(IT_df)
+
 
 # Dropping unuseful columns and rows
 HCLTECH = HCLTECH[['Date', 'Open', 'High', 'Low', 'Last', \
@@ -77,6 +87,41 @@ fig.update_layout(title={'text': "HCLTECH's Close Price & Volume Over Time",
                          'x':0.5,
                          'xanchor': 'center',
                          'yanchor': 'top'})
-plot(fig)
+#plot(fig)
 
 # Find the largest single-day increase and single-day decrease in close price
+
+
+# Second Research Question: Observe the trend in stock price for the entire IT
+# industry
+
+
+# find average of 5 stocks' close price over time
+avg_prices = IT_df.groupby('Date')['Close'].mean()
+
+fig2 = px.line(IT_df, x='Date', y='Close', color='Symbol')
+
+fig2.add_trace(go.Scatter(x=avg_prices.index,
+                         y=avg_prices,
+                         name='IT Industry',
+                         mode='lines',
+                         line=dict(color='firebrick')))
+
+fig2.update_layout(title={'text': "Stock Prices in IT Industry (2014-2019)",
+                         'y':0.94,
+                         'x':0.5,
+                         'xanchor': 'center',
+                         'yanchor': 'top'})
+#plot(fig2)
+
+# Compare the IT industry with other industries
+all_50 = pd.read_csv('./csv_files/NIFTY50_all.csv')
+all_50 = set_time(all_50)
+all_50 = all_50.merge(meta_data, left_on='Symbol', right_on='Symbol')
+avgs_all = all_50.groupby(['Industry', 'Date'])['Close'].mean()
+avgs_all = avgs_all.to_frame().reset_index()
+fig3 = px.line(avgs_all, x='Date', y='Close', color='Industry')
+
+plot(fig3)
+
+# Plot industries with similar development trend
