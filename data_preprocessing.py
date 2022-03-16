3c7017ef12eb7efc5ea26e390478d7b7be020887
zhiheng1-1927706 <97201955+zhiheng1-1927706@users.noreply.github.com>
2022-03-15 21:32:46 -0700

    Update data_preprocessing.py

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -8,6 +8,7 @@ data processing for later data analysis. The Pandas library is used here.
 import os
 import pandas as pd
 
+
 def csv_to_df():
     '''
     This function converts the csv files in the directory into dataframes
@@ -19,20 +20,20 @@ def csv_to_df():
     filenames_csv.sort()
     df_list = []
     for filename in filenames_csv:
-            path = os.path.join(directory_csv, filename)
-            company = filename[0:-4]
-            with open(path):
-                df = pd.read_csv(path)
-                if len(df) != 0:
-                    df['name'] = company
-                    df_list.append(df)
+        path = os.path.join(directory_csv, filename)
+        company = filename[0:-4]
+        with open(path):
+            df = pd.read_csv(path)
+            if len(df) != 0:
+                df['name'] = company
+                df_list.append(df)
     meta_data = df_list[-1]
     return meta_data, df_list, all_50
 
 
 def get_IT_df(meta_data, df_list):
     '''
-    This function gets individual dataframe for companies that are in 
+    This function gets individual dataframe for companies that are in
     the IT industry and returns each IT stock in dataframe
     '''
     IT_data = meta_data[meta_data['Industry'] == 'IT']
@@ -52,29 +53,27 @@ def get_IT_df(meta_data, df_list):
     WIPRO = set_time(WIPRO)
     IT_df = set_time(IT_df)
     # Dropping unuseful columns and rows for HCLTECH
-    HCLTECH = HCLTECH[['Date', 'Open', 'High', 'Low', 'Last', 'Prev Close',\
+    HCLTECH = HCLTECH[['Date', 'Open', 'High', 'Low', 'Last', 'Prev Close',
                        'Close', 'VWAP', 'Volume', 'Turnover', 'Trades']]
     return HCLTECH, INFY, TCS, TECHM, WIPRO, IT_df
 
 
 def set_time(df):
     '''
-    This function filters and returns data for year 2010-2018 (covid-19 imapct 
+    This function filters and returns data for year 2010-2018 (covid-19 imapct
     eliminated)
     '''
     time_mask = (df['Date'] <= '2018-12-31') & (df['Date'] >= '2010-1-1')
     df = df[time_mask]
     return df
 
+
 def main():
     meta_data, df_list, all_50 = csv_to_df()
-    
+
     # get the separate dataframe for each stock in the IT industry
     HCLTECH, INFY, TCS, TECHM, WIPRO, IT_df = get_IT_df(meta_data, df_list)
- 
-       
+
+
 if __name__ == '__main__':
     main()
-    
-    
-    
